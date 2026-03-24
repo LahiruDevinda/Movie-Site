642aced82580044f053867a91b12ca930150dc27
Lahiru <devindalahiru2004@gmail.com>
2026-03-24 17:46:42 +0530

    add filtering functionality for movies based on search and genre selection

---
+++ utils/filter.js
@@ -0,0 +1,66 @@
+import { fetchMovies } from "../data/movies.js";
+
+export let selectedGenres = [];
+
+let currentSearchTerm = '';
+let currentPage = 1;
+let isAdult = false;
+
+const genreMap = {
+    "Action": 28, "Adventure": 12, "Biography": 36, "Comedy": 35, 
+    "Crime": 80, "Drama": 18, "Fantasy": 14, "History": 36, 
+    "Horror": 27, "Sci-Fi": 878, "Thriller": 53
+};
+
+const searchBar = document.querySelector('.js-filter-search-bar');
+const genreButtons = document.querySelectorAll('.js-genre');
+const adultButton = document.querySelector('.js-adult-button');
+
+function handleSearch() {
+    const searchTerm = searchBar.value.trim();
+
+    if (searchTerm !== '') {
+        
+        currentSearchTerm = searchTerm;
+        currentPage = 1;
+        
+        fetchMovies(currentPage, currentSearchTerm);
+    } else {
+
+        currentSearchTerm = '';
+        currentPage = 1;
+        fetchMovies(currentPage, '');
+    }
+}
+
+searchBar.addEventListener('keypress', (event) => {
+    if (event.key === 'Enter') {
+        handleSearch();
+    }
+});
+
+genreButtons.forEach((button) => {
+    button.addEventListener('click', () => {
+        
+        const genreName = button.innerText;
+        const genreId = genreMap[genreName];
+
+        
+        if (!selectedGenres.includes(genreId)) {
+            selectedGenres.push(genreId);
+            button.classList.add('genre-active');
+        } else {
+            const index = selectedGenres.indexOf(genreId);
+            selectedGenres.splice(index, 1);
+            button.classList.remove('genre-active');
+        }
+        
+        currentPage = 1;
+        fetchMovies(currentPage);
+    });
+});
+
+adultButton.addEventListener('click', () => {
+    isAdult = !isAdult;
+    adultButton.classList.toggle('genre-active');
+});
