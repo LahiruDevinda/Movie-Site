35b1648a0dedbe4976ead1ef915f579628912f7d
Lahiru <devindalahiru2004@gmail.com>
2026-03-24 19:36:15 +0530

    add wishlist functionality to allow users to favorite movies

---
+++ data/wishlist.js
@@ -0,0 +1,50 @@
+import { ACCOUNT_ID,BEARER_TOKEN } from "./userData.js";
+
+const moviesContainer = document.querySelector('.movies-container'); 
+
+moviesContainer.addEventListener('click', async (event) => {
+    
+    const heartButton = event.target.closest('.add-to-wishlist');
+    if (!heartButton) return;
+
+    const movieId = heartButton.dataset.movieId;
+
+    const isAlreadyFavorited = heartButton.classList.contains('wishlist-active');
+
+    const newFavoriteState = !isAlreadyFavorited; 
+
+    try {
+        const options = {
+            method: 'POST',
+            headers: {
+                accept: 'application/json',
+                'content-type': 'application/json',
+                Authorization: `Bearer ${BEARER_TOKEN}`
+            },
+            body: JSON.stringify({
+                media_type: 'movie',
+                media_id: parseInt(movieId),
+                favorite: newFavoriteState
+            })
+        };
+
+        const response = await fetch(`https://api.themoviedb.org/3/account/${ACCOUNT_ID}/favorite`, options);
+        const data = await response.json();
+
+        if (data.success) {
+            
+            heartButton.classList.toggle('wishlist-active');
+            
+            if (newFavoriteState) {
+                console.log(`Added Movie ID ${movieId} to Favorites!`);
+            } else {
+                console.log(`Removed Movie ID ${movieId} from Favorites!`);
+            }
+        } else {
+            console.error('TMDB rejected the request:', data);
+        }
+
+    } catch (error) {
+        console.error('Error saving movie:', error);
+    }
+});
